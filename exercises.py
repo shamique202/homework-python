d5cac5ece2358a6372211d0218e47c5184288197
shamique202 <shamique.e@gmail.com>
2022-04-20 19:07:29 -0400

    E1

---
+++ exercises.py
@@ -0,0 +1,9 @@
+# Exercise 1
+
+# # Create a list named students containing some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+students = ['Shamique', 'Tim', 'Josh']
+print(students[1])
+print(students[-1])
